7379785f82b4b068d519d853ded65a2af6c74f5e
Ryuji TSUTSUI <ryu22e@gmail.com>
2023-08-26 15:10:46 +0900

    temp

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use actix_web::{web, App, HttpServer};
 use picot::routes::home;
+use picot::schema::setup_schema;
+use sea_orm::Database;
+use std::io::Error;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // TODO bookmark追加
+    let db = Database::connect("sqlite::memory:")
+        .await
+        .map_err(|e| Error::new(std::io::ErrorKind::Other, e))?;
+    setup_schema(&db)
+        .await
+        .map_err(|e| Error::new(std::io::ErrorKind::Other, e))?;
     HttpServer::new(|| App::new().route("/", web::get().to(home)))
         .bind(("127.0.0.1", 8080))?
         .run()

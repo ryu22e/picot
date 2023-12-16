f06f6f28c4c8a6b52aca99f278fa0586df9c107e
Ryuji TSUTSUI <ryu22e@gmail.com>
2023-12-16 16:37:21 +0900

    temp

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ use picot::schema::setup_schema;
 use sea_orm::Database;
 use std::io::Error;
 
+const SQLITE_FILE_PATH: &str = "/Users/ryu22e/development/picot/db.sqlite";
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    if !std::path::Path::new(SQLITE_FILE_PATH).exists() {
+        std::fs::File::create(SQLITE_FILE_PATH)?;
+    }
     // TODO ファイルパスを指定する
-    let db = Database::connect("sqlite::memory:")
+    let db = Database::connect(format!("sqlite://{}", SQLITE_FILE_PATH))
         .await
         .map_err(|e| Error::new(std::io::ErrorKind::Other, e))?;
     setup_schema(&db)

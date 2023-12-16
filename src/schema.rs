54cb73f3fd21b67860f2bb5cdbc764d8fc79ea64
Ryuji TSUTSUI <ryu22e@gmail.com>
2023-12-16 16:43:00 +0900

    temp

--- src/schema.rs
+++ src/schema.rs
@@ -2,9 +2,9 @@ use crate::entities::bookmark::Entity as Bookmark;
 use sea_orm::*;
 
 pub async fn setup_schema(db: &DbConn) -> Result<(), DbErr> {
-    // let builder = db.get_database_backend();
-    // let schema = Schema::new(builder);
-    // let st = builder.build(&schema.create_table_from_entity(Bookmark));
-    // db.execute(st).await?;
+    let builder = db.get_database_backend();
+    let schema = Schema::new(builder);
+    let st = builder.build(&schema.create_table_from_entity(Bookmark));
+    db.execute(st).await?;
     Ok(())
 }
